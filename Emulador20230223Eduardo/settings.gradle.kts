1ccdebc67be866d6f570fdb75060613022a532f1
Bryan Cornejo <20230223@ricaldone.edu.sv>
2024-05-01 21:49:55 -0600

    Emulador finalizado

---
+++ Emulador20230223Eduardo/settings.gradle.kts
@@ -0,0 +1,24 @@
+pluginManagement {
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Emulador20230223.Bryan"
+include(":app")
+ 
